a3c6873674ea7bb45a677453217812c77e7fac7e
Jake Cyr <cyrjake@gmail.com>
2024-03-21 23:12:14 -0400

    Move to examples folder

--- examples/mixtral.js
+++ examples/mixtral.js
@@ -1,13 +1,10 @@
-const { Tokenizer } = require('./tokenizer');
+const { Tokenizer } = require('../src/tokenizer');
 
 (async () => {
   const tokenizer = new Tokenizer('mistralai/Mixtral-8x7B-Instruct-v0.1');
   await tokenizer.load();
 
   const encoding = await tokenizer.encode('hey there how are you doing');
-
-  console.log(encoding);
-
   const decoded = await tokenizer.decode(encoding);
 
   console.log(decoded);
